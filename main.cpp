34f6180fe0c6e857fec5a2f60e0f468a57fe06c9
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-18 23:56:40 +0300

    конструкторы класса(реализация номер 2)

--- main.cpp
+++ main.cpp
@@ -6,26 +6,23 @@ public:
     Person(std::string n, int a)//конструктор класса;
     {
         name = n; age = a;
+        std::cout << "First constructor" << std::endl;
     }
-    Person(std::string n)
+    Person(std::string n): Person(n, 19)
     {
-        name = n;
-        age = 18;
+        std::cout << "Second constructor"<< std::endl;
     }
-    Person(){
-        name = "Bob";
-        age = 18;
+    Person(): Person("Alexa")
+    {
+        std::cout << "Third Constructor" << std::endl;
     }
+    //вызываем из одного конструктора другой
     void move(){
         std::cout << name << " is moving"<< std::endl;
     }
 };
 int main() {
-    Person person("Tom", 22);
-    std::cout << "Name: "<< person.name << "\tAge: " << person.age << std::endl;
-    Person sam("Sam");
-    std::cout << "Name: "<< sam.name << "\tAge: " << sam.age << std::endl;
-    Person Bob = Person();
-    std::cout << "Name: "<< Bob.name << "\tAge: " << Bob.age << std::endl;
+    Person Alexa = Person();
+    std::cout << "Name: "<< Alexa.name << "\tAge: " << Alexa.age << std::endl;
     return 0;
 }
