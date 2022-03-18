71c37464188297dd572b9cc5a7c361e6b88c4774
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-18 23:48:23 +0300

    конструкторы класса(реализация номер 1)

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@ class Person{
 public:
     std::string name;
     int age;
+    Person(std::string n, int a)//конструктор класса;
+    {
+        name = n; age = a;
+    }
+    Person(std::string n)
+    {
+        name = n;
+        age = 18;
+    }
+    Person(){
+        name = "Bob";
+        age = 18;
+    }
     void move(){
         std::cout << name << " is moving"<< std::endl;
     }
 };
 int main() {
-    Person person;
-    Person *ptr = &person;
-    ptr->name = "Tom";
-    ptr->age = 22;
-    ptr->move();
-    std::cout << "Name: " << ptr->name << "\tAge: "<<ptr->age << std::endl;
-    std::cout << "Name: " << person.name << "\tAge: "<<person.age << std::endl;//Изменения по указателю ptr в данном случае приведут к изменениям объекта person.
+    Person person("Tom", 22);
+    std::cout << "Name: "<< person.name << "\tAge: " << person.age << std::endl;
+    Person sam("Sam");
+    std::cout << "Name: "<< sam.name << "\tAge: " << sam.age << std::endl;
+    Person Bob = Person();
+    std::cout << "Name: "<< Bob.name << "\tAge: " << Bob.age << std::endl;
     return 0;
 }
