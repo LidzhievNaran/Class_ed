ad33f2805efe3732469f2169098499bdfe87c82e
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-19 00:32:58 +0300

    вынос реализации методов класса во вне

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 class Person{
 public:
-    const std::string name;
-    int &ageRef;
-    int age;
-    Person(std::string n, int a) : name(n), age(a), ageRef(age)//когда код инструкции из тела конструктора начнут выполняться, константы и ссылки уже должны быть инициализированы
-    {
-    }
-    Person(std::string n) : name(n), age(18), ageRef(age)
-    {
-    }
-    Person():name("Alexa"), age(19), ageRef(age)
-    {
-    }
-    void move(){
-        std::cout << name << "is moving"<< std::endl;
-    }
+ std::string name;
+ int age;
+ Person(std::string n, int a);
+ void move();
 };
+
+Person::Person(std::string n, int a)
+{
+    name = n; age = a;
+}
+void Person::move(){
+ std::cout << name << " is moving"<< std::endl;
+}
 int main() {
     Person tom("Tom", 22);
-    std::cout << "Name: "<< tom.name<< "\tAge: "<< tom.age<< "\t AgeRef: "<< tom.ageRef << std::endl;
-    Person Alexa = Person();
-    std::cout << "Name: "<< Alexa.name<< "\tAge: "<< Alexa.age<< "\t AgeRef: "<< Alexa.ageRef << std::endl;
+    std::cout << "Name: " << tom.name << "\tAge: " << tom.age << std::endl;
+    tom.move();
     return 0;
 }
