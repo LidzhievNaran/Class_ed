c5ff999503159ceb9758291defaafaf77696a20b
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-19 00:41:05 +0300

    инкапсуляция

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
-class Person{
+
+class Person {
 public:
- std::string name;
- int age;
- Person(std::string n, int a);
- void move();
+    Person(std::string n, int a);
+    void move();
+    void setAge(int a);
+    std::string getName();
+    int getAge();
+private:
+    std::string name;
+    int age;
 };
 
-Person::Person(std::string n, int a)
-{
-    name = n; age = a;
+Person::Person(std::string n, int a) {
+    name = n;
+    age = a;
 }
-void Person::move(){
- std::cout << name << " is moving"<< std::endl;
+void Person::setAge(int a) {
+    if (a> 0 && a < 100) age = a;
+}
+void Person::move() {
+    std::cout << name << " is moving" << std::endl;
+}
+std::string Person::getName() {
+    return name;
+}
+int Person::getAge(){
+    return age;
 }
 int main() {
     Person tom("Tom", 22);
-    std::cout << "Name: " << tom.name << "\tAge: " << tom.age << std::endl;
-    tom.move();
+    std::cout << "Name: " << tom.getName() << "\tAge: " << tom.getAge() << std::endl;
+    tom.setAge(31);
+    std::cout << "Name: " << tom.getName() << "\tAge: " << tom.getAge() << std::endl;
+    tom.setAge(291);
+    std::cout << "Name: " << tom.getName() << "\tAge: " << tom.getAge() << std::endl;
     return 0;
 }
