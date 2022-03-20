281a5b25cb005923093609714bde53560760dd25
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-20 19:53:26 +0300

    перегрузка операторов(2)

--- main.cpp
+++ main.cpp
@@ -11,16 +11,23 @@ public:
     {
         std::cout << seconds << " seconds"<< std::endl;
     }
+    Counter operator + (Counter c2){
+        return (this->seconds + c2.seconds);
+    }
+    int operator + (int s)
+    {
+        return this->seconds + s;
+    }
     int seconds;
 };
-Counter operator + (Counter c1, Counter c2)
-{
-    return Counter(c1.seconds + c2.seconds);
-}
+
 int main() {
     Counter c1(20);
     Counter c2(10);
+    Counter fuck(90);
     Counter c3 = c1 + c2;
     c3.display();
+    int seconds = c1 +25;
+    std::cout << seconds << std::endl;
     return 0;
 }
