c2d45e0a679aaf475c2f5f727cd6ac1d606cd065
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-20 19:23:58 +0300

    const static

--- main.cpp
+++ main.cpp
@@ -3,17 +3,10 @@
 
 class Account{
 public:
+    const static int rate = 8;
     Account(double sum)
     {
         this->sum = sum;
-    }
-    static int getRate()
-    {
-        return rate;
-    }
-    static void setRate(int r)
-    {
-        rate = r;
     }
     double getIncome()
     {
@@ -21,15 +14,11 @@ public:
     }
 private:
     double sum;
-    static int rate;
 };
-int Account::rate = 8;
 int main() {
     Account account1(20000);
     Account account2(50000);
-    Account::setRate(5);
-    std::cout << "Rate: "<< Account::getRate() << std::endl;
-    std::cout << "Rate: "<< account1.getRate() << " Income: "<< account1.getIncome() << std::endl;
-    std::cout << "Rate: "<< account2.getRate()<< " Income: "<< account2.getIncome() << std::endl;
+    std::cout << "Rate: "<< account1.rate << "\tIncome: " << account1.getIncome()<< std::endl;
+    std::cout << "Rate: "<< account2.rate << "\tIncome: " << account2.getIncome()<< std::endl;
     return 0;
 }
