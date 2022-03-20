284fcda9b94d9dc6c06205ca9a17ade09ddb1feb
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-20 20:04:28 +0300

    перегрузка операторов(операторы присвоения)

--- main.cpp
+++ main.cpp
@@ -11,37 +11,18 @@ public:
     {
         std::cout << seconds << " seconds"<< std::endl;
     }
-    Counter operator + (Counter c2){
-        return (this->seconds + c2.seconds);
-    }
-    int operator + (int s)
+    Counter& operator += (Counter c2)
     {
-        return this->seconds + s;
+        seconds += c2.seconds;
+        return *this;
     }
     int seconds;
 };
-bool operator == (Counter c1, Counter c2)
-{
-    return c1.seconds == c2.seconds;
-}
-bool operator != (Counter c1, Counter c2)
-{
-    return c1.seconds != c2.seconds;
-}
-bool operator > (Counter c1, Counter c2){
-    return c1.seconds > c2.seconds;
-}
-bool operator < (Counter c1, Counter c2){
-    return c1.seconds < c2.seconds;
-}
 
 int main() {
     Counter c1(20);
     Counter c2(10);
-    bool b1 = c1 == c2;
-    bool b2 = c1 > c2;
-    std::cout << b1 << std::endl;
-    std::cout << b2 << std::endl;
-
+    c1 += c2;
+    c1.display();
     return 0;
 }
