b3dc728ac21c982b473104a5ea3b9274da28def0
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-04-12 01:23:28 +0300

    класс с шаблоном)

--- main.cpp
+++ main.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
 #include <string>
-class IntAccount
-{
+template <typename T>
+class Account {
 private:
-    int id;
+    T id;
 public:
-    IntAccount(int id): id(id)
-    { }
-    int getId(){
-        return  id;
-    }
-};
+    Account (T id ): id (id)
+    {
 
-class StrAccount {
-private:
-    std::string id;
-public:
-    StrAccount(std::string id) : id (id)
-    {}
-    std::string getId(){
+    }
+    T getId(){
         return id;
     }
 };
 
 int main() {
-    StrAccount acc1("ca-pub1292342340");
-    IntAccount acc2(12343452);
-    std::cout << "acc1: "<< acc1.getId()<< std::endl;
+    Account <std::string> acc1("ca-pub21232131");
+    Account <int> acc2(2131231232131);
+    std::cout << "acc1: "<< acc1.getId() << std::endl;
     std::cout << "acc2: "<< acc2.getId() << std::endl;
     return 0;
 }
